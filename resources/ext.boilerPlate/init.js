36885d71f2ac157d721955f914af2da0b8b90eac
christian birego <93344052+birego@users.noreply.github.com>
2024-05-12 18:54:24 +0200

    add Button takeSummary

--- resources/ext.boilerPlate/init.js
+++ resources/ext.boilerPlate/init.js
@@ -1,5 +1,11 @@
 function logAndStoreURL() {
-    var currentPageURL = window.location.href;
-    console.log(currentPageURL);
+  const currentPageURL = window.location.href;
+  console.log(currentPageURL);
 }
 logAndStoreURL();
+function takeSummary() {
+  console.log("Le résumé a été pris !");
+}
+const button = document.querySelector(".mw-htmlform-submit");
+
+button.addEventListener("click", takeSummary);
