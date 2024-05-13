e29a54fe32be57d32fc043be99a02a31222a0e73
BirushaNdegeya <birushandegeya@gmail.com>
2024-05-13 14:21:09 +0200

    fix license MIT

--- resources/ext.boilerPlate/init.js
+++ resources/ext.boilerPlate/init.js
@@ -3,9 +3,12 @@ function logAndStoreURL() {
   console.log(currentPageURL);
 }
 logAndStoreURL();
+
 function takeSummary() {
   console.log("Le résumé a été pris !");
 }
 const button = document.querySelector(".mw-htmlform-submit");
 
 button.addEventListener("click", takeSummary);
+
+const div = document.createElement('div');
